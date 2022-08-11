ec8361e5b9ada1d305864dbc7a50b4e096405b84
PuspitaSwain01 <Puspitaswain01@gamil.com>
2022-08-12 00:25:39 +0530

    Project added to central Repository

---
+++ src/main/java/com/Nitu/SpringBootCurdWithRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.Nitu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdWithRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdWithRestFullApplication.class, args);
+	}
+
+}
